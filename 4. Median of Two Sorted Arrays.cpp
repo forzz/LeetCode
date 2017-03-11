acd76e13a69481fe8a184660c81acf20a823ed9a
forzz <forzfuyao@gmail.com>
2017-03-11 14:50:51 +0800

    Update 4. Median of Two Sorted Arrays.cpp

--- 4. Median of Two Sorted Arrays.cpp
+++ 4. Median of Two Sorted Arrays.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size();
         for(int i=0;i<nums2.size();i++)
             nums1.push_back(nums2[i]);
-        sort(nums1.begin(),nums1.end());
+        inplace_merge(nums1.begin(),nums1.begin()+n,nums1.end());
         return nums1.size()%2==0 ?(nums1[nums1.size()/2] + nums1[nums1.size()/2 -1]) /double(2):nums1[nums1.size()/2];
     }
 };
